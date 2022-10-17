5d68b098cd224e7c5f7e72a804f68d4f537a8ca6
Munise Ben <munise0704@gmail.com>
2022-10-17 07:13:45 +0200

    new details add

--- manager.js
+++ manager.js
@@ -2,20 +2,36 @@
 
 console.log("Welcome to your task manager, Press: \n 1. to see all your tasks\n 2. to add a task\n 3. to delete a task\n 4. to mark a task as done\n 5. to Exit the task manager");
 
-const file_name = "tasks.json" // file name i ilerde degistirirsek kolaylik olur.
-const fs = require('fs');
-const readline = require('readline');
-var user_input = readline.createInterface(process.stdin, process.stdout);
+const file_name = "tasks.json" // json'i ilerde degistirirsek kolaylik olur diye file name degiskenine atadik./ string
+const fs = require('fs');  // //js'nin kendi icinde external kutp var. Herh bir dosyadan acma, okuma, yazma yapmak icin. Dosya sist.(kutuphane)
+const readline = require('readline');//terminalden line okuyan baska bir kutuphane.
+var rl = readline.createInterface(process.stdin, process.stdout); // stdin = standart input // readline kutuphanesinin objesi demek ama bunu biz kendimiz belirledik
 
-function see_your_tasks(){
-    fs.readFile( file_name , function(err, file){
+const showtasks = () => {
+    fs.readFile( file_name , (err, data) => { //rf fonskuyonunun ikinci parametresi iki parametreli bi fonksyon biri err digeri file yani dosyanin icinde yazan tum data
         if (err) throw err;
-        let tasks = JSON.parse(file);
+        let tasks = JSON.parse(data); //jsondan okumus oldugum string veya array vya frkli bi data tipi
         console.log(tasks);
     });
 }
 
-// let data = JSON.stringify(student, null, 2);
+const addtask= ()=> {
+    var data = "";
+    rl.question("task_name :",(task)=>{
+        data = task;
+    })
+    console.log(data);
+
+    fs.writeFile(file_name, JSON.stringify(data), err => {  // let data = JSON.stringify(student);
+        if (err) throw err;
+    });
+}
+
+// fs.writeFile("./customer.json", JSON.stringify(customer), err => {
+//     if (err) console.log("Error writing file:", err);
+//   });
+////////////////////////////////////
+// let data = JSON.stringify(student);
 
 // fs.writeFile('student-3.json', data, (err) => {
 //     if (err) throw err;
@@ -24,18 +40,18 @@ function see_your_tasks(){
 
 // console.log('This is after the write call');
 
+const start_task_manager = () => {
 
-function start_task_manager(){
+    rl.question("selection :", //terminalde cikacak bir soru ve inputu gosteriyor.//onceden inputu html uzerinden aliyorduk simdi js uzerinden aldik. // burada iki parametre var ikinsisi fonksyonumz
+    function(selection){ // (selection)=>{
 
-    user_input.question("selection :",
-    function(selection){
         if (selection == 1){
-            see_your_tasks();
-            start_task_manager();
+            showtasks();
+            start_task_manager(); //recursive function call, kendisini tekrar etmk.
 
         }
         else if (selection == 2){
-            console.log(2);
+            addtask();
             start_task_manager();
         }
 
@@ -50,7 +66,7 @@ function start_task_manager(){
         }
 
         else if (selection==5){
-            user_input.close();
+            rl.close();
         }
 
         else {
@@ -69,7 +85,7 @@ start_task_manager();
 
 // == esittir demek degil
 // console.log(selection);
-//     user_input.close();
+//     rl.close();
 
 //  if(3<5 && 1<3){
 //     console.log("uc besten kucuktur");
@@ -94,7 +110,7 @@ start_task_manager();
 // }
 
 // write2screen(5);
-//key : value
+//key : value jsonla alakali
 
 
 
---
+++ note_yasin.js
@@ -0,0 +1,11 @@
+// require js nin kendi kutuphanesini import ediyor.
+// const fs = require('fs');
+// const readline = require('readline');
+// js nin kendi icinde external kutuph var. bunlari kullanmak icin reguire ediyorum. adini istedgim gb degstreblr.
+// herhangi bir dosyadan acma okuma yazma yapmak istiyorsak. dosya sistemi. bu fonksyonn gerekli .
+// o yuzden requerler dosyanin en basina yaziilir
+
+// readline diye ayri bi kuutphane var terminalden line okuyyor.
+//terminal = standar input output demektir. yani buradan oku yaz diyor. genelde bu boyle.
+
+//parametreye noye iht yok?
